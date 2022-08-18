e3280ad4c8a3903e363cd73869cb8cedb9484ffb
Nolan Kingdon <Nolan.Kingdon@D2L.com>
2022-08-18 11:12:53 -0400

    Updating todos

--- src/extension.ts
+++ src/extension.ts
@@ -10,15 +10,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// - Documentation
 	// - Save state so we don't have to reload everything every time we click
 	// - Styles 
+	//		- CSS
+	//		- Better Icon
 	// - Actual Tree
 	//		- Delete nodes
-	//		- Navigate to nodes (Re-request probably best)
+	//		- Short URL display
 	// - Content View
-	//		- Inputs
-	//		- Display: Siren Entity structure
+	//		- Inputs need to work
+	//		- Ensure all possible information is represented
 	// - Deactivate functions?
 	// - Error Handling (Bad requests, events, etc.)
 	//		- If we 401, we need to be shown that upfront and loudly
+	// - Named content pages (Display in tree)
+
+	// TODO - known bugs
+	//	- Navigating to an existing base entity re-creates that base entity
+	//	- using .map() causes commas to be placed in between all content
+	//	- Content is not saved when navigating off of the tabs
 }
 
 export function deactivate() {
