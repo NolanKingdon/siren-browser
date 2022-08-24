f38f9e3a637cb4c954b43609f7e96053348305a0
Nolan Kingdon <Nolan.Kingdon@D2L.com>
2022-08-24 16:55:15 -0400

    Update todos

--- src/extension.ts
+++ src/extension.ts
@@ -7,15 +7,18 @@ export function activate(context: vscode.ExtensionContext) {
 	app = new SirenBrowser(context);
 
 	// TODO 
+	// - ReadMe
 	// - Documentation
 	// - Content View
 	//		- Collapsable Sections
 	//		- Ensure all possible information is represented
 	// - Deactivate functions?
 	// - Named content pages (Display in tree)
-	// - Try extracting JS as much out of the template stuff as possible
-	//		- import JS files saved elsewhere for example
 	// - Refactoring
+	// 		- Try extracting JS as much out of the template stuff as possible
+	//			- import JS files saved elsewhere for example
+	// - Hotkeys
+	// - Right click, search selected
 }
 
 export function deactivate() {
